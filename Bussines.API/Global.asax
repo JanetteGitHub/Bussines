3741de013b76eb0cbd80f2087c4dbf37600bd6cd
sbakD3 <47613427+sbakD3@users.noreply.github.com>
2019-08-26 16:38:08 -0500

    Arquitectura

---
+++ Bussines.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bussines.API.WebApiApplication" Language="C#" %>
---
+++ Bussines.Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bussines.Backend.Startup))]
+namespace Bussines.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
